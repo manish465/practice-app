43c204798e32874b1758dbd5a370eab4744340df
manish465 <ms5978307@gmail.com>
2020-03-26 14:13:08 -0700

    final

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -3,11 +3,31 @@ import { Jumbotron as Jumbo, Container } from "react-bootstrap";
 import styled from "styled-components";
 import img from "../assets/MSG.jpg";
 
-const Styled = styled.div``;
+const Styled = styled.div`
+    .jumbo {
+        background: url(${img}) no-repeat fixed bottom;
+        background-size: cover;
+        color: #efefef;
+        height: 200px;
+        position: relative;
+        z-index: -2;
+    }
+
+    .overlay {
+        background-color: #000;
+        opacity: 0.6;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        z-index: -1;
+    }
+`;
 
 const Jumbotron = () => (
     <Styled>
-        <Jumbo fluid className="jubo">
+        <Jumbo fluid className="jumbo">
             <div className="overlay" />
             <Container>
                 <h1>Welcom</h1>
